ec8af7f2909781f7c656676420159a54a05897ce
Oguzhan <47873038+oguzthmc@users.noreply.github.com>
2022-06-04 01:29:52 +0300

    create chakra theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
 
 const Home: NextPage = () => {
   return (
     <div>Hello</div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
