ddd7dfbbbdc70e3a0c3c8d58380c545c03f5dfad
Oguzhan <47873038+oguzthmc@users.noreply.github.com>
2022-06-06 00:27:48 +0300

    auth buttons & chakra ui global component styling

---
+++ src/components/Navbar/RightContent/AuthButtons.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Button } from '@chakra-ui/react';
+
+const AuthButtons: React.FC = () => {
+  return (
+    <>
+      <Button
+        variant='outline'
+        height='28px'
+        display={{ base: 'none', sm: 'flex' }}
+        width={{ base: '70x', md: '110px' }}
+        mr={2}
+      // onClick={() => {}}
+      >
+        Log In
+      </Button>
+      <Button
+        height='28px'
+        display={{ base: 'none', sm: 'flex' }}
+        width={{ base: '70px', md: '110px' }}
+        mr={2}
+      // onClick={() => {}}
+      >
+        Sign Up
+      </Button>
+    </>
+  );
+};
+export default AuthButtons;
