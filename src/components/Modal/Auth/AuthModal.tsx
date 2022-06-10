0ffafd04f57301cd8e2ff8be62d71a4938a0a6b6
Oguzhan <47873038+oguzthmc@users.noreply.github.com>
2022-06-10 14:41:22 +0300

    useauthstate firebase hook

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -11,21 +11,29 @@ import {
   Flex,
   Text
 } from '@chakra-ui/react';
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useRecoilState } from 'recoil';
 import { authModalState } from '../../../atoms/authModalAtom';
+import { auth } from '../../../firebase/clientApp';
 import AuthInputs from './AuthInputs';
 import OAuthButtons from './OAuthButtons';
 
 const AuthModal: React.FC = () => {
   const [modalState, setModalState] = useRecoilState(authModalState);
+  const [user, loading, error] = useAuthState(auth);
 
-  const handleClose = () => {
+  const handleClose = useCallback(() => {
     setModalState((prev) => ({
       ...prev,
       open: false,
     }));
-  };
+  }, [setModalState]);
+
+  useEffect(() => {
+    if (user) handleClose();
+    console.log("user", user);
+  }, [user, handleClose]);
 
   return (
     <>
--- src/components/Navbar/RightContent/RightContent.tsx
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import { Flex } from '@chakra-ui/react';
+import { Button, Flex } from '@chakra-ui/react';
 import AuthModal from '../../Modal/Auth/AuthModal';
 import AuthButtons from './AuthButtons';
+import { signOut } from 'firebase/auth';
+import { auth } from '../../../firebase/clientApp';
 
 type RightContentProps = {
-  // user: any;
+  user: any;
 };
 
-const RightContent: React.FC<RightContentProps> = () => {
+const RightContent: React.FC<RightContentProps> = ({ user }) => {
   return (
     <>
       <AuthModal />
       <Flex justify='center' align='center'>
-        <AuthButtons />
+        {user ? (
+          <Button onClick={() => signOut(auth)}>Logout</Button>
+        ) : (
+          <AuthButtons />
+        )}
       </Flex>
     </>
   );
