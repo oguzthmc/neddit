2937533967f247d0d9ca92c570084e084f9eef26
Oguzhan <47873038+oguzthmc@users.noreply.github.com>
2022-06-13 23:54:55 +0300

    user dropdown menu

--- src/components/Navbar/RightContent/RightContent.tsx
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -5,6 +5,7 @@ import AuthButtons from './AuthButtons';
 import { signOut, User } from 'firebase/auth';
 import { auth } from '../../../firebase/clientApp';
 import Icons from './Icons';
+import UserMenu from './UserMenu';
 
 type RightContentProps = {
   user?: User | null;
@@ -16,7 +17,7 @@ const RightContent: React.FC<RightContentProps> = ({ user }) => {
       <AuthModal />
       <Flex justify='center' align='center'>
         {user ? <Icons /> : <AuthButtons />}
-        {/* <Menu /> */}
+        <UserMenu user={user} />
       </Flex>
     </>
   );
