882ba136130039051748a359e0d5673ccc922ba3
Oguzhan <47873038+oguzthmc@users.noreply.github.com>
2022-06-19 00:54:17 +0300

    community directory in navbar

--- src/components/Navbar/SearchInput.tsx
+++ src/components/Navbar/SearchInput.tsx
@@ -6,14 +6,20 @@ import {
   InputLeftElement
 } from '@chakra-ui/react';
 import { SearchIcon } from '@chakra-ui/icons';
+import { User } from 'firebase/auth';
 
 type SearchInputProps = {
-  // user: 
+  user?: User | null;
 };
 
-const SearchInput: React.FC<SearchInputProps> = () => {
+const SearchInput: React.FC<SearchInputProps> = ({ user }) => {
   return (
-    <Flex flexGrow={1} mr={2} align='center'>
+    <Flex
+      flexGrow={1}
+      maxWidth={user ? 'auto' : '600px'}
+      mr={2}
+      align='center'
+    >
       <InputGroup>
         <InputLeftElement
           pointerEvents='none'
