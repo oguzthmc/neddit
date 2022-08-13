33ac652dac7b176e5834695070b0fc98f0831bc2
Oguzhan <47873038+oguzthmc@users.noreply.github.com>
2022-08-13 17:23:00 +0300

    community page header component

--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -5,6 +5,7 @@ import { Community } from '../../../atoms/communitiesAtom';
 import { firestore } from '../../../firebase/clientApp';
 import safeJsonStringfy from 'safe-json-stringify';
 import NotFound from '../../../components/Community/NotFound';
+import Header from '../../../components/Community/Header';
 
 type CommunityPageProps = {
   communityData: Community;
@@ -17,7 +18,11 @@ const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
     return <NotFound />;
   };
 
-  return <div>WELCOME TO {communityData.id}</div>;
+  return (
+    <>
+      <Header communityData={communityData} />
+    </>
+  );
 };
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
