3cfb376fe893d6f378dc5152bd1596e47a7b3ba5
Oguzhan <47873038+oguzthmc@users.noreply.github.com>
2022-08-14 22:05:45 +0300

    fetch user community snippets

--- src/hooks/useCommunityData.tsx
+++ src/hooks/useCommunityData.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useRecoilState } from 'recoil';
-import { Community, communityState } from '../atoms/communitiesAtom';
+import { Community, CommunitySnippet, communityState } from '../atoms/communitiesAtom';
+import { auth, firestore } from '../firebase/clientApp';
 
 const useCommunityData = () => {
+  const [user] = useAuthState(auth);
+
   const [communityStateValue, setcommunityStateValue] = useRecoilState(communityState);
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   const onJoinOrLeaveCommunity = (
     communityData: Community,
@@ -19,14 +27,42 @@ const useCommunityData = () => {
     joinCommunity(communityData);
   };
 
+  const getMySnippets = async () => {
+    setLoading(true);
+    try {
+      // get users snippets
+      const snippetDocs = await getDocs(
+        collection(firestore, `users/${user?.uid}/communitySnippets`)
+      );
+
+      const snippets = snippetDocs.docs.map(doc => ({ ...doc.data() }));
+      setcommunityStateValue(prev => ({
+        ...prev,
+        mySnippets: snippets as Array<CommunitySnippet>,
+      }));
+
+      console.log('here are snippets', snippets);
+    } catch (error) {
+      console.log('getMySnippets error', error);
+    }
+    setLoading(false);
+  };
+
   const joinCommunity = (communityData: Community) => { };
 
   const leaveCommunity = (communityId: string) => { };
+
+  useEffect(() => {
+    if (!user) return;
+    getMySnippets();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user]);
 
   return {
     // data and functions
     communityStateValue,
     onJoinOrLeaveCommunity,
+    loading,
   };
 };
 export default useCommunityData;
