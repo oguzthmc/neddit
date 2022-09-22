4a884015131b502f8f085bab052e59de70235ef5
Oguzhan <47873038+oguzthmc@users.noreply.github.com>
2022-09-22 14:07:37 +0300

    customize community image

--- src/components/Posts/NewPostForm.tsx
+++ src/components/Posts/NewPostForm.tsx
@@ -24,6 +24,7 @@ import { BsLink45Deg, BsMic } from 'react-icons/bs';
 import { IoDocumentText, IoImageOutline } from 'react-icons/io5';
 import { Post } from '../../atoms/postsAtom';
 import { firestore, storage } from '../../firebase/clientApp';
+import useSelectFile from '../../hooks/useSelectFile';
 import ImageUpload from './PostForm/ImageUpload';
 import TextInputs from './PostForm/TextInputs';
 import TabItem from './TabItem';
@@ -67,7 +68,7 @@ const NewPostForm: React.FC<NewPostFormProps> = ({ user }) => {
     title: '',
     body: '',
   });
-  const [selectedFile, setSelectedFile] = useState<string>();
+  const { selectedFile, setSelectedFile, onSelectFile } = useSelectFile();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
@@ -110,20 +111,6 @@ const NewPostForm: React.FC<NewPostFormProps> = ({ user }) => {
     setLoading(false);
   };
 
-  const onSelectImage = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const reader = new FileReader();
-
-    if (event.target.files?.[0]) {
-      reader.readAsDataURL(event.target.files[0]);
-    }
-
-    reader.onload = (readerEvent) => {
-      if (readerEvent.target?.result) {
-        setSelectedFile(readerEvent.target.result as string);
-      }
-    };
-  };
-
   const onTextChange = (
     event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
@@ -154,7 +141,7 @@ const NewPostForm: React.FC<NewPostFormProps> = ({ user }) => {
         {selectedTab === 'Images & Video' && (
           <ImageUpload
             selectedFile={selectedFile}
-            onSelectImage={onSelectImage}
+            onSelectImage={onSelectFile}
             setSelectedTab={setSelectedTab}
             setSelectedFile={setSelectedFile}
           />
